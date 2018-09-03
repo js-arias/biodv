ca15fe0d4f4acd2f6c67422111a31a6fa39220d9
J. Salvador Arias <jsalarias@gmail.com>
2018-09-03 14:54:09 -0300

    records: misspell corrected

--- records/records_test.go
+++ records/records_test.go
@@ -252,6 +252,6 @@ func TestTaxonList(t *testing.T) {
 		t.Errorf("lines beginning with '#' should be left unread")
 	}
 	if _, ok := db.tids[biodv.TaxCanon("; Indominus rex")]; ok {
-		t.Errorf("lines bigining with ';' should be left unread")
+		t.Errorf("lines beginning with ';' should be left unread")
 	}
 }
